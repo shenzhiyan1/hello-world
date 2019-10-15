31a1e28b5283be2e15a30c278d720f7a0c7b5da9
shenzhiyan1 <55731707+shenzhiyan1@users.noreply.github.com>
2019-10-15 14:18:41 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -10,5 +10,11 @@ curl=curl_easy_init();
 if(curl){
 
 curl_easy_setopt(curl,CURLOPT_URL,"http://myip.ipip.net/");
-res=curl_easy_
+res=curl_easy_perform(curl);
 if(res!=CURLE_OK)
+  fprintf(stderr,"curl_easy_perform()failed:%s\n"),
+  curl_easy_strerror(res));
+  curl_easy_cleanup(curl);
+}
+  return0;
+}
